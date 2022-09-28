b3c118bc0e6bb27aa81e17db5499087045049423
Mats Daniel Larsen <matsl@aboveit.no>
2022-09-28 14:47:01 +0200

    wrote test to get each block list.

---
+++ test/index.test.js
@@ -0,0 +1,16 @@
+const { getBlockList, BlockList } = require('../index.js');
+
+var assert = require('assert');
+describe('getBlockList', function () {
+  it('should get ads list', async function () {
+    const list = await getBlockList(BlockList.ADS);
+    assert.equal(list.length > 0, true);
+  });
+
+  it('should get a list for every BlockList enum', async function () {
+    for (const listName in BlockList) {
+      const list = await getBlockList(BlockList[listName]);
+      assert.equal(list.length > 0, true);
+    }
+  });
+});
